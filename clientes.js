0ef427b9ed837137236055237485b1255c3b7b5a
kleecollage <56179611+kleecollage@users.noreply.github.com>
2021-04-22 01:41:09 -0500

    Update clientes.js
    cambia la forma de añadir los clientes al localstorage
    le añadi funcion para borrar local storage
    añadi funcion para copiar datos de localstorage a una tabla

--- clientes.js
+++ clientes.js
@@ -1,27 +1,51 @@
-let nuevoCliente = [] ;
 
-const addClient = (ev) =>{
-    ev.preventDefault() ;
+    var arr = new Array() ;
 
-let $cliente = {
-    rfc: document.getElementById("rfc").value ,
-    razonSc: document.getElementById("razonSc").value ,
-    direccionFisc: document.getElementById("direccionFisc").value ,
-    email: document.getElementById("email").value 
+
+    function addData(){
+        getData() ;
+        arr.push({
+            rfc: document.getElementById("rfc").value ,
+            razonSc: document.getElementById("razonSc").value ,
+            direccionFisc: document.getElementById("direccionFisc").value ,
+            email: document.getElementById("email").value 
+        }) ;
+        localStorage.setItem("localData" , JSON.stringify(arr)) ;
+
+    }
+
+    function showData(){
+        getData() ;
+        var tbl = document.getElementById("MyTable")
+
+        for (var i = 0 ; i < arr.length ; i++){
+            var row = tbl.insertRow();
+            var cell1 = row.insertCell();
+            var cell2 = row.insertCell();
+            var cell3 = row.insertCell();
+            var cell4 = row.insertCell();
+
+            cell1.innerHTML = arr[i].rfc ;
+            cell2.innerHTML = arr[i].razonSc ;
+            cell3.innerHTML = arr[i].direccionFisc ;
+            cell4.innerHTML = arr[i].email ;
+        }
+    }
+
+    function getData(){
+        var str = localStorage.getItem("localData") ;
+        if (str != null){
+            arr = JSON.parse(str)
+        }
+    }
+
+    function deleteStorage(){
+        localStorage.clear()
+        var elmtTable = document.getElementById('myTable');
+        var tableRows = elmtTable.getElementsByTagName( 'tr');
+        var rowCount = tableRows.length;
+
+        for (var x=rowCount-1; x>0; x--) {
+        elmtTable.removeChild(tableRows[x]);
 }
-    nuevoCliente.push($cliente) ; 
-    document.forms[0].reset() ;
-
-    console.warn('cliente añadido al localstorage' , nuevoCliente)
-
-
-    localStorage.setItem("cliente" , JSON.stringify(nuevoCliente))
-   
-    //Verificación
-    //let pre = document.querySelector('#msg pre')  
-    //pre.textContent = '\n' + JSON.stringify(nuevoCliente , '\t' , 2 )
-}
-
-    document.addEventListener("DOMContentLoaded",  ()=>{
-        document.getElementById('btnRegistro').addEventListener("click" , addClient)
-    })
+    }   
